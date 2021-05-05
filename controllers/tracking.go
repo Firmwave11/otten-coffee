5c50b20c062e37f8a26a6be66bc344f9cb5e12be
Rama Sapto <ramasapto3@gmail.com>
2021-05-05 17:21:47 +0700

    handle error

--- controllers/tracking.go
+++ controllers/tracking.go
@@ -10,14 +10,15 @@ func (u *ctrl) Tracking(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 	ctx, res, code, err := u.uc.Tracking(ctx)
 
+	datares, err := json.Marshal(res)
+
 	if err != nil || code >= 400 {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(code)
-		fmt.Fprintf(w, err.Error())
+		fmt.Fprintf(w, string(datares))
 		return
 	}
 
-	datares, err := json.Marshal(res)
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
 	fmt.Fprintf(w, string(datares))
